2a349cce5724210208ce1f319446e718d2d0e0ab
Daniel Agurcia <danielagurcia90@gmail.com>
2019-06-07 13:37:18 -0600

    commit 1

---
+++ Main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <string>
+using std::string;
+using std::stringstream;
+
+
+
+int main(){
+    int opcion=0;
+        while(opcion!=4){
+                cout<<"Menu"<<endl
+                <<"1-Ejercicio #1"<<endl
+                <<"2-Ejercicio #2"<<endl
+                <<"3-Salir"<<endl;
+                cout<<"Ingrese una opcion: "<<endl;
+                cin>>opcion;
+                switch (opcion){
+                    case 1:
+
+                    break;
+                    case 2:
+
+                    break;
+                }
+        }
+    return 0;
+}
